f257c6edd63d89e29a2d4db17cf9f5e726328a71
leen <leenodeh287@gmail.com>
2023-10-24 19:15:56 +0300

    add imges to hero-page

--- src/Pages/Home/Carousel.jsx
+++ src/Pages/Home/Carousel.jsx
@@ -1,19 +1,15 @@
-import heroCarousel1 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
-import heroCarousel2 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
-import heroCarousel3 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
-import heroCarousel4 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
-import heroCarousel5 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
-
+import { assets } from '../../data/Carousal';
 
 const Carousel = () => {
   return (
     <div className="slider-wrapper">
-<div className='slider-content'>
-
-
-</div>
+      <div className='slider-content'>
+        {assets.map((e, index) => (
+          <img src={e.url} alt={`carousal-img-${index}`} key={index} />
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
---
+++ src/data/Carousal.jsx
@@ -0,0 +1,25 @@
+//using cloudinary to convert img-static to url
+export const assets= [
+    {
+      
+     url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel4_z4w1cg.jpg"
+    },
+    {
+    
+     url:
+        " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel3_ocdh30.jpg",
+    },
+    {
+    
+      url: "https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel1_rg5b2z.jpg",
+    },
+    {
+      
+      url:" https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel2_oijmah.jpg"
+    },
+    {
+   
+      url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel5_vydgki.jpg",
+    },
+  ];
+  
