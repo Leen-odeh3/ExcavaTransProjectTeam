11deda29e6fb820dda3638680d49341609b490d2
leen <leenodeh287@gmail.com>
2023-10-24 20:12:07 +0300

    update all component

--- src/Pages/Home/Carousel.jsx
+++ src/Pages/Home/Carousel.jsx
@@ -1,12 +1,22 @@
-import { assets } from '../../data/Carousal';
+import heroCarousel1 from '../../assets/HeroCarousel/heroCarousel1.jpg'
+import heroCarousel2 from '../../assets/HeroCarousel/heroCarousel2.jpg'
+import heroCarousel3 from '../../assets/HeroCarousel/heroCarousel3.jpg'
+import heroCarousel4 from '../../assets/HeroCarousel/heroCarousel4.jpg'
+import heroCarousel5 from '../../assets/HeroCarousel/heroCarousel5.jpg'
+
+
+import './Slider.scss'
 
 const Carousel = () => {
   return (
     <div className="slider-wrapper">
-      <div className='slider-content'>
-        {assets.map((e, index) => (
-          <img src={e.url} alt={`carousal-img-${index}`} key={index} />
-        ))}
+      <div className='slider-content'> 
+      <img src={heroCarousel1} alt={`img ${heroCarousel1}`}/>
+      <img src={heroCarousel2}  alt={`img ${heroCarousel2}`}/>
+      <img src={heroCarousel3}  alt={`img ${heroCarousel3}`}/>
+      <img src={heroCarousel4}  alt={`img ${heroCarousel4}`}/>
+      <img src={heroCarousel5}  alt={`img ${heroCarousel5}`}/>
+
       </div>
     </div>
   );
--- src/data/Carousal.jsx
+++ src/data/Carousal.jsx
@@ -1,25 +1,19 @@
 //using cloudinary to convert img-static to url
-export const assets= [
-    {
-      
-     url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel4_z4w1cg.jpg"
-    },
-    {
-    
-     url:
-        " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel3_ocdh30.jpg",
-    },
-    {
-    
-      url: "https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel1_rg5b2z.jpg",
-    },
-    {
-      
-      url:" https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel2_oijmah.jpg"
-    },
-    {
-   
-      url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel5_vydgki.jpg",
-    },
-  ];
-  
+export const assets = [
+  {
+    url: "https://res.cloudinary.com/dll0c6zey/image/upload/v1698166290/heroCarousel1_eje9pw.jpg",
+  },
+  {
+    url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel5_vydgki.jpg",
+  },
+  {
+    url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel4_z4w1cg.jpg",
+  },
+  {
+    url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel3_ocdh30.jpg",
+  },
+
+  {
+    url: " https://res.cloudinary.com/dll0c6zey/image/upload/v1698163121/HeroCarousel/heroCarousel2_oijmah.jpg",
+  },
+];
