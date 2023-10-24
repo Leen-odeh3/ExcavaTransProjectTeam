7c13e07b7443b590b74f68223d478ae76b061f88
leen <leenodeh287@gmail.com>
2023-10-24 18:54:46 +0300

    adding assets

--- src/Pages/Home/Carousel.jsx
+++ src/Pages/Home/Carousel.jsx
@@ -1,8 +1,17 @@
+import heroCarousel1 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
+import heroCarousel2 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
+import heroCarousel3 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
+import heroCarousel4 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
+import heroCarousel5 from '../../assets/HeroCarousel/hero-carousel-1.jpg';
+
 
 const Carousel = () => {
   return (
     <div className="slider-wrapper">
-   
+<div className='slider-content'>
+
+
+</div>
     </div>
   )
 }
