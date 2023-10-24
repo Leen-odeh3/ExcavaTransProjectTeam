6c85ec712a6ebbc1c84fc31807d3d4fd844d446c
leen <leenodeh287@gmail.com>
2023-10-24 15:40:56 +0300

    responsive topHeader done

--- src/Component/Header/TopHeader.jsx
+++ src/Component/Header/TopHeader.jsx
@@ -4,7 +4,6 @@ import { Social } from "../../data/Social";
 const TopHeader = () => {
   return (
     <div className="top-header">
-        
       <div className="top-header-place">
         <i className="bi bi-geo-alt-fill"></i>
         1095 Howard Street, San Francisco, USA
@@ -18,7 +17,7 @@ const TopHeader = () => {
         <i className="bi bi-envelope-fill"></i>
         info@excava.com
       </div>
-     
+
       <div className="top-header-links">
         {Social.map((socialLink, index) => (
           <a key={index} href={socialLink.url} target="-blank">
