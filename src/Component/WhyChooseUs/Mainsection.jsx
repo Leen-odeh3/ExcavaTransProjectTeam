15862ced927d916246f1760bd932be2fcf84c2bc
leen <leenodeh287@gmail.com>
2023-11-02 02:56:03 +0300

    done section

--- src/Component/WhyChooseUs/Mainsection.jsx
+++ src/Component/WhyChooseUs/Mainsection.jsx
@@ -1,4 +1,5 @@
 import "./Mainsection.scss";
+import { reasons } from "../../data/Reasons";
 
 const Mainsection = () => {
   return (
@@ -10,10 +11,19 @@ const Mainsection = () => {
           <span> </span>
         </div>
 
-        <p> INDUSTRY LEADER IN CONSTRUCTION EQUIPMENT SUPPLY</p>
+        <p>INDUSTRY LEADER IN CONSTRUCTION EQUIPMENT SUPPLY</p>
       </div>
 
-      <div className="reasons-section"></div>
+      <div className="reasons-section">
+        {reasons.map((e, index) => (
+          <div className="index" key={index}>
+            <img src={e.url} alt={e.title} />
+            <h3>{e.title}</h3>
+            <p>{e.desc}</p>
+           
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/data/Reasons.jsx
+++ src/data/Reasons.jsx
@@ -1,16 +1,16 @@
 export const reasons = [
     {
-     url: ,
+     url: "https://res.cloudinary.com/dll0c6zey/image/upload/v1698880904/Screenshot_2023-11-02_022008_btoyea.png",
     title: "Quality Equipment",
     desc:"Offering guarantees an agreed price for all covered repairs, allowing you to budget carefully and avoid unexpected expense."
     },
     {
-     url: ,
+     url: "https://res.cloudinary.com/dll0c6zey/image/upload/v1698880914/Screenshot_2023-11-02_022028_czlyhk.png",
 title:"Fast Service",
       desc: "Providing a unique insight into the health of your machine, helping to avoid expensive repairs and unexpected downtime.",
     },
   {   
-    url: ,
+    url: "https://res.cloudinary.com/dll0c6zey/image/upload/v1698880915/Screenshot_2023-11-02_022046_mgtxrl.png",
   title:"Support 24/7",
         desc: "Regular, planned Care Inspection gives you the knowledge you need to ensure that your machine is always in top shape on the world.",
       },
