1d6438ba8a015084ff088e10feeede802d7ba350
leen <leenodeh287@gmail.com>
2023-11-02 02:24:19 +0300

    add all data

--- src/Component/WhyChooseUs/Mainsection.jsx
+++ src/Component/WhyChooseUs/Mainsection.jsx
@@ -1,24 +1,21 @@
-import './Mainsection.scss'
+import "./Mainsection.scss";
 
 const Mainsection = () => {
   return (
-    <div className='main-section'>
-      <div className='info-section'>
+    <div className="main-section">
+      <div className="info-section">
         <div>
-         <span> </span>
-        <b>REASONS TO CHOOSE US</b>
-        <span> </span>   
+          <span> </span>
+          <b>REASONS TO CHOOSE US</b>
+          <span> </span>
         </div>
-        
-        <p> INDUSTRY LEADER IN CONSTRUCTION EQUIPMENT SUPPLY
-</p>
-         </div>
 
-      <div className='reasons-section'>
-        
-     </div>
+        <p> INDUSTRY LEADER IN CONSTRUCTION EQUIPMENT SUPPLY</p>
+      </div>
+
+      <div className="reasons-section"></div>
     </div>
-  )
-}
+  );
+};
 
-export default Mainsection
+export default Mainsection;
---
+++ src/data/Reasons.jsx
@@ -0,0 +1,18 @@
+export const reasons = [
+    {
+     url: ,
+    title: "Quality Equipment",
+    desc:"Offering guarantees an agreed price for all covered repairs, allowing you to budget carefully and avoid unexpected expense."
+    },
+    {
+     url: ,
+title:"Fast Service",
+      desc: "Providing a unique insight into the health of your machine, helping to avoid expensive repairs and unexpected downtime.",
+    },
+  {   
+    url: ,
+  title:"Support 24/7",
+        desc: "Regular, planned Care Inspection gives you the knowledge you need to ensure that your machine is always in top shape on the world.",
+      },
+  ];
+  
