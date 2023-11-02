93ce40c27c65ed5c1fc04c950673010f2737d4ee
leen <leenodeh287@gmail.com>
2023-11-02 14:06:48 +0300

    footer structure

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,10 +1,26 @@
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
-  )
-}
+    <div className="footer">
+      <div className="footer-section-one">
+        <div className="footer-info">
+          <p className="sub">Subscribe to Our Newsletter</p>
+          <p style={{color:"#ADADAD"}}>
+            Want to stay up to date on our work and latest news? Subscribe to
+            our newsletters
+          </p>
+        </div>
 
-export default Footer
+        <form>
+          <div className="form">
+            <input type="email" placeholder="Email" />
+            <button>Subscibe</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
